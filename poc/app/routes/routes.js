d8576d8ca3102217ce22874aa6d1369322d2dda9
fabien.daoulas <fabien.daoulas@alten.nl>
2016-03-15 10:32:46 +0100

    * routing added * split callbacks to requests in other files to ease testing

--- poc/app/routes/routes.js
+++ poc/app/routes/routes.js
@@ -1,32 +1,13 @@
 /**
     Expose API routes 
 */
-var consoleLogger = require('../logger/logger');
+var consoleLogger = require('../logger/logger'),
+    teamsRoutes = require('./teams.routes.js'),
+    playersRoutes = require('./players.routes.js'),
+    championsRoutes = require('./champions.routes.js');
 
 module.exports = function(app) {
-
-    app.get('/champions', (req, res) => {
-        res.send({});//TODO    
-    });
-    
-    app.get('/players', (req, res) => {
-        res.send({});//TODO    
-    });
-    
-    app.get('/teams/:teamid', (req, res) => {
-        res.send({id:req.params.teamid});//TODO     
-    });
-    
-    app.get('/players/:playerid', (req, res) => {
-        res.send({id: req.params.playerid});//TODO    
-    });
-    
-    app.get('/players/:playerid/affinityPlayer/:otherplayerid', (req, res) => {
-        res.send([{id:req.params.playerid},{id:req.params.otherplayerid}]);//TODO    
-    });
-    
-    app.get('/players/:playerid/affinityChampion/:championid', (req, res) => {
-        res.send([{id:req.params.playerid},{id:req.params.championid}]);//TODO    
-    });
-    
+    app.use('/players', playersRoutes);
+    app.use('/champions', championsRoutes);
+    app.use('/teams', teamsRoutes);
 };
