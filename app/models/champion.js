88902e9d6c612839cb9dfd0cb50ffae1ce267f86
Gollwu <jeanfrancois.aubry0@gmail.com>
2016-03-15 18:25:04 +0100

    bugfix and connection to DB

--- app/models/champion.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var ChampionSchema = mongoose.Schema({
-    name: String,
-    ap: Number,
-    ad: Number
-});
-
-module.exports = ChampionSchema;
--- config/initDB.js
+++ config/initDB.js
@@ -1,33 +1,60 @@
 var env = process.env.NODE_ENV || 'development';
 var config = require('./config')[env];
 
+var consoleLogger = require('../app/logger/logger');
+var Promise = require('promise');
+
 var mongoose = require("mongoose"),
     Schema = mongoose.Schema,
     relationship = require("mongoose-relationship");
 	
 mongoose.connect('mongodb://' + config.database.host + config.database.port + config.database.db);
 
-Player = require('../app/models/player'),
-Champion = require('../app/models/champion');
+Player = require('../app/models/playerModel'),
+Champion = require('../app/models/championModel');
 
-var player1 = new Player({
-    name: "Jefe",
-    team: "TeamSoloJefe",
-    championsAffinity: [{name: "Rammus", affinity: 100}],
-    playersAffinity: [{name: "Daoulas", affinity: 12}]
-});
-player1.save();
+var promise = setupDB();
+promise
+	.then(() => {
+		consoleLogger.info('DB connection and init successful.');				
+	})
+	.catch((err) => {
+		consoleLogger.info('Failed to open a connection to database or save data. ', err);
+		process.exit(1)
+	});
+	
+function setupDB() {
+    var promise =
+        new Promise((fulfill, reject) => {
+            try{
+                var player1 = new Player({
+					name: "Jefe",
+					team: "TeamSoloJefe",
+					championsAffinity: [{name: "Rammus", affinity: 100}],
+					playersAffinity: [{name: "Daoulas", affinity: 12}]
+				});
+				player1.save();
 
-var player2 = new Player({
-    name: "Daoulas",
-    team: "TeamSoloJefe",
-    championsAffinity: [{name: "Rammus", affinity: 12}],
-    playersAffinity: [{name: "Daoulas", affinity: 11}]
-});
-player2.save();
+				var player2 = new Player({
+					name: "Daoulas",
+					team: "TeamSoloJefe",
+					championsAffinity: [{name: "Rammus", affinity: 12}],
+					playersAffinity: [{name: "Daoulas", affinity: 11}]
+				});
+				player2.save();
 
-var champion1 = new Champion({ name: "Rammus", strength:100 });
-champion1.save();
+				var champion1 = new Champion({ name: "Rammus", strength:100 });
+				champion1.save();               
+                fulfill();
+            } 
+            catch(err) {
+                reject(err);    
+            }
+        });
+    return promise;
+}	
+
+
 
 
 
