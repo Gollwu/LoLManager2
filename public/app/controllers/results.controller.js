ea1571c1f660451aa7652341154750ba04267bea
Gollwu <jeanfrancois.aubry0@gmail.com>
2016-05-18 01:00:09 +0200

    Calculations for kills and deaths on result page

--- public/app/controllers/results.controller.js
+++ public/app/controllers/results.controller.js
@@ -4,19 +4,20 @@ module.exports = function(app) {
     app.controller('ResultsCtrl', ($scope, $http, $timeout, champSelectService) => {
 	
 		$scope.blueTeam = "Team Solo Jefe";
-		$scope.redTeam = "Counter Daoulas Gaming";	
-		
-        
+		$scope.redTeam = "Counter Daoulas Gaming";	        
 		
         $scope.initResults = function() {			
-			var promises = champSelectService.setAffinities();
+			var promises = champSelectService.setAffinitiesAndPerformances();
 			Promise.all(promises)
 				.then(() => {	
 					// get teams champion select service    
 					$scope.blueTeamPlayers = champSelectService.getBlueTeamPlayers();	
-					$scope.redTeamPlayers = champSelectService.getRedTeamPlayers();				
-					$scope.$apply();		
-					console.log($scope.blueTeamPlayers)					
+					$scope.redTeamPlayers = champSelectService.getRedTeamPlayers();		
+					
+					champSelectService.assignScores();					
+					console.log("WTFFFFFFFF");	
+					console.log("BlueTeam : " + $scope.blueTeamPlayers);					
+					$scope.$apply();									
 				})
 				.catch((err) => {
 					reject(err);
--- public/app/services/champSelect.service.js
+++ public/app/services/champSelect.service.js
@@ -4,55 +4,102 @@ module.exports = function(app) {
 			data: [{
 			  name: '',
 			  champion: '',
-			  team: 'blue',
-			  affinity: 0
+			  team: 'Blue',
+			  role: 'Top',			  
+			  performance:0,
+			  kills:0,
+			  deaths:0,
+			  assists:0,
 			},{
 			  name: '',
 			  champion: '',
-			  team: 'red',
-			  affinity: 0
+			  team: 'Red',		
+			  role: 'Top',				  
+			  performance:0,
+			  kills:0,
+			  deaths:0,
+			  assists:0
 			},{
 			  name: '',
 			  champion: '',
-			  team: 'red',
-			  affinity: 0
+			  team: 'Red',
+			  role: 'Jungler',		
+			  performance:0,
+			  kills:0,
+			  deaths:0,
+			  assists:0
 			},{
 			  name: '',
 			  champion: '',
-			  team: 'blue',
-			  affinity: 0
+			  team: 'Blue',	
+		      role: 'Jungler',
+			  performance:0,
+			  kills:0,
+			  deaths:0,
+			  assists:0
 			},{
 			  name: '',
 			  champion: '',
-			  team: 'blue',
-			  affinity: 0
+			  team: 'Blue',
+			  role: 'Mid',	
+			  performance:0,
+			  kills:0,
+			  deaths:0,
+			  assists:0
 			},{
 			  name: '',
 			  champion: '',
-			  team: 'red',
-			  affinity: 0
+			  team: 'Red',
+			  role: 'Mid',			  
+			  performance:0,
+			  kills:0,
+			  deaths:0,
+			  assists:0
 			},{
 			  name: '',
 			  champion: '',
-			  team: 'blue',
-			  affinity: 0
+			  team: 'Red',
+			  role: 'Marksman',
+			  performance:0,
+			  kills:0,
+			  deaths:0,
+			  assists:0
 			},{
 			  name: '',
 			  champion: '',
-			  team: 'blue',
-			  affinity: 0
+			  team: 'Blue',
+			  role: 'Marksman',			  
+			  performance:0,
+			  kills:0,
+			  deaths:0,
+			  assists:0
 			},{
 			  name: '',
 			  champion: '',
-			  team: 'red',
-			  affinity: 0
+			  team: 'Blue',	
+			  role: 'Support',			
+			  performance:0,
+			  kills:0,
+			  deaths:0,
+			  assists:0
 			},{
 			  name: '',
 			  champion: '',
-			  team: 'red',
-			  affinity: 0
+			  team: 'Red',	
+			  role: 'Support',				  
+			  performance:0,
+			  kills:0,
+			  deaths:0,
+			  assists:0
 			}],
+			//ID of the player currently picking
 			playerPicking:0,
+			//Ratios of KDA by role
+			KDARatios: [{role:"Top", kill:"20", death:"25", assist:"25"}, 
+						{role:"Jungler", kill:"15", death:"20", assist:"25"}, 
+						{role:"Mid", kill:"30", death:"15", assist:"15"}, 
+						{role:"Marksman", kill:"30", death:"15", assist:"15"}, 
+						{role:"Support", kill:"5", death:"25", assist:"30"}],
 			// Receives the name of both team and updates the player names for the champ select
 			fillPlayers: function(blueTeamName, redTeamName) {		
 				var promises = [];
@@ -141,6 +188,12 @@ module.exports = function(app) {
 			  redPlayers.push(this.data[9]);
 			  return redPlayers;
 			},
+			getRedTeamTotalPerformance: function() { 
+			  return (this.data[1].performance + this.data[2].performance + this.data[5].performance + this.data[6].performance + this.data[9].performance)			
+			},
+			getBlueTeamTotalPerformance: function() { 
+			  return (this.data[0].performance + this.data[3].performance + this.data[4].performance + this.data[7].performance + this.data[8].performance)			
+			},
 			//Return a list of player objects for red team
 			getBlueTeamPlayers: function() {		  
 			  var bluePlayers = [];
@@ -153,7 +206,7 @@ module.exports = function(app) {
 			},
 			
 			//Set the affinities of the players with their champion once champ select is done
-			setAffinities: function() {		  
+			setAffinitiesAndPerformances: function() {		  
 				var promises = [];
 			    this.data.forEach(function a(player,index) {
 					promises.push($http({
@@ -162,13 +215,101 @@ module.exports = function(app) {
 					})
 					.then((response) => {
 						response.data.championsAffinity.forEach(function b(champion) {
-							if(champion.name==player.champion){
-								player.affinity = champion.affinity
+							if(champion.name==player.champion){								
+								player.performance = Math.round((Math.random()*100))+champion.affinity;
 							}
 						});					
 					}));	
 				}); 
 				return promises;
+			},
+			assignScores: function() {
+				//Put a game length between 25 and 60 min
+				//TODO Parametrize
+				var gameLength = Math.random()*35+25;
+				var killPerMinutes = Math.random()+0.1;
+				var totalKills = Math.round(gameLength * killPerMinutes);
+				
+				console.log("gameLength : " + gameLength);
+				console.log("killPerMinutes : " + killPerMinutes);
+				console.log("totalKills : " + totalKills);
+				
+				var redTeamPerformance = this.getRedTeamTotalPerformance();
+				var blueTeamPerformance = this.getBlueTeamTotalPerformance();		
+
+				console.log("redTeamPerformance : " + redTeamPerformance);
+				console.log("blueTeamPerformance : " + blueTeamPerformance);
+				
+				var winningTeam = (Math.random()*(redTeamPerformance+blueTeamPerformance)<redTeamPerformance ? "Red" : "Blue");
+				
+				console.log("Winning Team : " + winningTeam);				
+				
+				var winningTeamKillPercentageBonus = 30;
+				var blueTeamKills = Math.round((totalKills*redTeamPerformance)/(redTeamPerformance+blueTeamPerformance)) 
+				var redTeamKills = Math.round((totalKills*blueTeamPerformance)/(redTeamPerformance+blueTeamPerformance)) 
+
+				console.log("blueTeamKills : " + blueTeamKills);
+				console.log("redTeamKills : " + redTeamKills);						
+											
+				if(winningTeam == "Blue"){
+					blueTeamKills += Math.round(blueTeamKills/10); 					
+				}else{
+					redTeamKills += Math.round(redTeamKills/10); 	
+				}
+				
+				console.log("blueTeamKills : " + blueTeamKills);
+				console.log("redTeamKills : " + redTeamKills);		
+				
+				//TODO Set Total assist multiplicator with team affinity?
+				var blueTeamAssists = blueTeamKills*3;
+				var redTeamAssists = redTeamKills*3;
+				
+				console.log("blueTeamAssists : " + blueTeamAssists);		
+				console.log("redTeamAssists : " + redTeamAssists);		
+				
+				//Set KDA percentages per blue player
+				totalBlueKill = this.KDARatios[0].kill * (this.data[0].performance*100/blueTeamPerformance) + this.KDARatios[1].kill * (this.data[3].performance*100/blueTeamPerformance) + this.KDARatios[2].kill * (this.data[4].performance*100/blueTeamPerformance) + this.KDARatios[3].kill * (this.data[7].performance*100/blueTeamPerformance) + this.KDARatios[4].kill * (this.data[8].performance*100/blueTeamPerformance)
+				totalBlueAssist = this.KDARatios[0].assist * (this.data[0].performance*100/blueTeamPerformance) + this.KDARatios[1].assist * (this.data[3].performance*100/blueTeamPerformance) + this.KDARatios[2].assist * (this.data[4].performance*100/blueTeamPerformance) + this.KDARatios[3].assist * (this.data[7].performance*100/blueTeamPerformance) + this.KDARatios[4].assist * (this.data[8].performance*100/blueTeamPerformance)	
+					
+				//Set KDA percentages per red player
+				totalRedKill = this.KDARatios[0].kill * (this.data[1].performance*100/redTeamPerformance) + this.KDARatios[1].kill * (this.data[2].performance*100/redTeamPerformance) + this.KDARatios[2].kill * (this.data[5].performance*100/redTeamPerformance) + this.KDARatios[3].kill * (this.data[6].performance*100/redTeamPerformance) + this.KDARatios[4].kill * (this.data[9].performance*100/redTeamPerformance)				
+				totalRedAssist = this.KDARatios[0].assist * (this.data[1].performance*100/redTeamPerformance) + this.KDARatios[1].assist * (this.data[2].performance*100/redTeamPerformance) + this.KDARatios[2].assist * (this.data[5].performance*100/redTeamPerformance) + this.KDARatios[3].assist * (this.data[6].performance*100/redTeamPerformance) + this.KDARatios[4].assist * (this.data[9].performance*100/redTeamPerformance)
+				
+				//Set kills for blue team						
+				this.data[0].kills = Math.round(((this.KDARatios[0].kill * (this.data[0].performance*100/blueTeamPerformance))/totalBlueKill)*blueTeamKills);
+				this.data[3].kills = Math.round(((this.KDARatios[1].kill * (this.data[3].performance*100/blueTeamPerformance))/totalBlueKill)*blueTeamKills);
+				this.data[4].kills = Math.round(((this.KDARatios[2].kill * (this.data[4].performance*100/blueTeamPerformance))/totalBlueKill)*blueTeamKills);
+				this.data[7].kills = Math.round(((this.KDARatios[3].kill * (this.data[7].performance*100/blueTeamPerformance))/totalBlueKill)*blueTeamKills);
+				this.data[8].kills = Math.round(((this.KDARatios[4].kill * (this.data[8].performance*100/blueTeamPerformance))/totalBlueKill)*blueTeamKills);	
+				
+				//Set assists for blue team						
+				this.data[0].assists = Math.round(((this.KDARatios[0].assist * (this.data[0].performance*100/blueTeamPerformance))/totalBlueAssist)*blueTeamAssists);
+				this.data[3].assists = Math.round(((this.KDARatios[1].assist * (this.data[3].performance*100/blueTeamPerformance))/totalBlueAssist)*blueTeamAssists);
+				this.data[4].assists = Math.round(((this.KDARatios[2].assist * (this.data[4].performance*100/blueTeamPerformance))/totalBlueAssist)*blueTeamAssists);
+				this.data[7].assists = Math.round(((this.KDARatios[3].assist * (this.data[7].performance*100/blueTeamPerformance))/totalBlueAssist)*blueTeamAssists);
+				this.data[8].assists = Math.round(((this.KDARatios[4].assist * (this.data[8].performance*100/blueTeamPerformance))/totalBlueAssist)*blueTeamAssists);	
+				
+				//Set kills for red team						
+				this.data[1].kills = Math.round(((this.KDARatios[0].kill * (this.data[1].performance*100/redTeamPerformance))/totalRedKill)*redTeamKills);
+				this.data[2].kills = Math.round(((this.KDARatios[1].kill * (this.data[2].performance*100/redTeamPerformance))/totalRedKill)*redTeamKills);
+				this.data[5].kills = Math.round(((this.KDARatios[2].kill * (this.data[5].performance*100/redTeamPerformance))/totalRedKill)*redTeamKills);
+				this.data[6].kills = Math.round(((this.KDARatios[3].kill * (this.data[6].performance*100/redTeamPerformance))/totalRedKill)*redTeamKills);
+				this.data[9].kills = Math.round(((this.KDARatios[4].kill * (this.data[9].performance*100/redTeamPerformance))/totalRedKill)*redTeamKills);	
+				
+				//Set assists for red team						
+				this.data[1].assists = Math.round(((this.KDARatios[0].assist * (this.data[1].performance*100/redTeamPerformance))/totalRedAssist)*redTeamAssists);
+				this.data[2].assists = Math.round(((this.KDARatios[1].assist * (this.data[2].performance*100/redTeamPerformance))/totalRedAssist)*redTeamAssists);
+				this.data[5].assists = Math.round(((this.KDARatios[2].assist * (this.data[5].performance*100/redTeamPerformance))/totalRedAssist)*redTeamAssists);
+				this.data[6].assists = Math.round(((this.KDARatios[3].assist * (this.data[6].performance*100/redTeamPerformance))/totalRedAssist)*redTeamAssists);
+				this.data[9].assists = Math.round(((this.KDARatios[4].assist * (this.data[9].performance*100/redTeamPerformance))/totalRedAssist)*redTeamAssists);	
+
+				
+				
+				
+				//TODO verify amount of kills and equalize
+				
+				return winningTeam;
+			
 			}
 		};	
 	});
